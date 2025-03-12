fe9f946de75fc35ba9d207ba76588bb5efbb76df
khsujon <kamrulhasan.km3@gmail.com>
2025-03-12 22:48:44 +0600

    offer screen done

--- lib/screens/offer_screen/offer_screen.dart
+++ lib/screens/offer_screen/offer_screen.dart
@@ -1,5 +1,6 @@
 import 'package:amar_wallet/constants/app_colors.dart';
 import 'package:amar_wallet/utils/media_query_utils.dart';
+import 'package:amar_wallet/widgets/offer_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -60,91 +61,17 @@ class _OfferScreenState extends State<OfferScreen> {
               Expanded(
                 child: ListView(
                   children: [
-                    Container(
-                      padding: const EdgeInsets.all(16),
-                      height: MediaQueryUtils.screenHeight * 0.25,
-                      width: MediaQueryUtils.screenWidth,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: Color.fromRGBO(45, 44, 44, 1),
-                        border: Border.all(
-                          color: Color.fromRGBO(255, 255, 255, 0.2),
-                          width: 1,
-                        ),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              const Text(
-                                'Duncof',
-                                style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const Spacer(),
-                              Container(
-                                height: 40,
-                                width: 40,
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 8, vertical: 4),
-                                decoration: BoxDecoration(
-                                  color: Color.fromRGBO(0, 0, 0, 0.3),
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: Image.asset(
-                                    "assets/images/mynaui_coffee.png"),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            '10% Discount on your coffee (up to BDT 100) minimum order of BDT 299',
-                            style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white,
-                            ),
-                          ),
-                          const Spacer(),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 12, vertical: 8),
-                                decoration: BoxDecoration(
-                                  color: Color.fromRGBO(68, 68, 68, 1),
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: const Text(
-                                  'Add Promo',
-                                  style: TextStyle(
-                                    fontFamily: 'Sen',
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              const Text(
-                                'Valid till March 31, 2025',
-                                style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                    OfferCard(),
+                    SizedBox(
+                      height: MediaQueryUtils.screenHeight * 0.02,
+                    ),
+                    OfferCard(),
+                    SizedBox(
+                      height: MediaQueryUtils.screenHeight * 0.02,
+                    ),
+                    OfferCard(),
+                    SizedBox(
+                      height: MediaQueryUtils.screenHeight * 0.02,
                     ),
                   ],
                 ),
